c6d1a44c28eb9701a547c870e1bb5976b5aaa317
Muhammad Sheraz Khilji <muhammadsherazkhan104@gmail.com>
2025-04-05 12:15:47 +0500

    Custom payment gatway Init  : Custom payment Gatway is initilization

---
+++ wp-custom-wallet-pro/wp-custom-wallet-pro.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP Custom Wallet Pro
+Plugin URI: https://yourwebsite.com/
+Description: A custom wallet system for Easypaisa , JazzCash for secure transections.
+Version: 1.0
+Author: Exarth
+*/
+
+
+defined('ABSPATH') || exit;
