6da811683b448decfac8dc34f3b57795cd1ba1f7
Muhammad Sheraz Khilji <muhammadsherazkhan104@gmail.com>
2025-04-05 13:09:28 +0500

    Plugin Actiavtion  : plugin actiavtion hook is added

--- wp-custom-wallet-pro/wp-custom-wallet-pro.php
+++ wp-custom-wallet-pro/wp-custom-wallet-pro.php
@@ -9,3 +9,21 @@ Author: Exarth
 
 
 defined('ABSPATH') || exit;
+register_activation_hook(__FILE__, 'exarth_create_payment_table');
+function exarth_create_payment_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'custom_payments';
+    $charset_collate = $wpdb->get_charset_collate();
+    
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        amount varchar(255) NOT NULL,
+        phone varchar(20) NOT NULL,
+        gateway varchar(50) NOT NULL,
+        status varchar(20) NOT NULL,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+    
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
